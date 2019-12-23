d11933c56832e33e014b2240934c37d0ea85daf7
saloed <bogdan19021998ua@gmail.com>
2019-12-23 12:45:25 +0200

    Add Index ( html + controller )

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,26 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Data;
+
+import java.util.Objects;
+
 //@Entity
+@Data
 public class BaseEntity {
 
     private Long id;
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
